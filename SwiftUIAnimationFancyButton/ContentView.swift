84ddc736437dd8a6942b89483243c3b32da78b98
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-08-31 02:05:20 +0300

    Initial Commit

---
+++ SwiftUIAnimationFancyButton/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIAnimationFancyButton
+//
+//  Created by Abdelrahman Mohamed on 31.08.2020.
+//  Copyright © 2020 Abdelrahman Mohamed. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
